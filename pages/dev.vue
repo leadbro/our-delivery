78fce336a576a9da3369e66f99a72ada388534c2
Alexey Andreev <leadbro@rambler.ru>
2018-09-16 17:48:11 +0300

    Создал ветку

---
+++ pages/dev.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page-dev">
+    <base-pagination-numbered
+        class="our-services-layer-top__pagination"
+        :current-id="currentItemId"
+        :count="_items.length"
+        @set-index="onPaginationClick"
+    />
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      currentItemId: 0
+    }
+  },
+  components: {
+
+  },
+  computed: {
+    ...mapGetters({
+      _items: 'services/items',
+      getItemById: 'services/getItemById',
+    }),
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/mixins.scss";
+
+  .page-dev {
+
+  }
+
+</style>
+
